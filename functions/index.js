fb2bc6a4a2d14808897bcf251bc6e8b0647b39eb
adilcse <adil.cs.work@gmail.com>
2020-03-16 14:41:05 +0530

    auto update added

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,14 @@ const functions = require('firebase-functions');
 const admin=require('firebase-admin');
 const algoliasearch=require('algoliasearch')
 const ALGOLIA_APP_ID='R0CZB88STM';
-const ALGOLIA_ADMIN_KEY=process.env.NODE_APP_ALGOLIA_ADMIN_KEY;
+const ALGOLIA_ADMIN_KEY='';
 const ALGOLIA_INDEX_NAME='product_name';
 const ALGOLIA_INDEX_SELLER='sellers';
+const client=algoliasearch(ALGOLIA_APP_ID,ALGOLIA_ADMIN_KEY);
 admin.initializeApp(functions.config().firebase);
+
 exports.addFirebaseDataToAlgolia=functions.https.onRequest((req,res)=>{
-    var products=[];
+    let products=[];
    
     // eslint-disable-next-line promise/catch-or-return
     admin.firestore().collection('products').get().then(docs=>{
@@ -18,8 +20,8 @@ exports.addFirebaseDataToAlgolia=functions.https.onRequest((req,res)=>{
             product.objectID=doc.id;
             products.push(product);
         });
-        var client=algoliasearch(ALGOLIA_APP_ID,ALGOLIA_ADMIN_KEY);
-       var index=client.initIndex(ALGOLIA_INDEX_NAME);
+       
+       const index=client.initIndex(ALGOLIA_INDEX_NAME);
        index.saveObjects(products,(err,content) => {
            res.status(200).send(content);
        })
@@ -30,7 +32,7 @@ exports.addFirebaseDataToAlgolia=functions.https.onRequest((req,res)=>{
 //add seller data to algolia
 
 exports.addSellerDataToAlgolia=functions.https.onRequest((req,res)=>{
-    var sellers=[];
+    let sellers=[];
    
     // eslint-disable-next-line promise/catch-or-return
     admin.firestore().collection('seller').get().then(docs=>{
@@ -45,11 +47,53 @@ exports.addSellerDataToAlgolia=functions.https.onRequest((req,res)=>{
             };
             sellers.push(seller);
         });
-        var client=algoliasearch(ALGOLIA_APP_ID,ALGOLIA_ADMIN_KEY);
-       var index=client.initIndex(ALGOLIA_INDEX_SELLER);
+       const index=client.initIndex(ALGOLIA_INDEX_SELLER);
        index.saveObjects(sellers,(err,content) => {
            res.status(200).send(content);
        })
 
     })
+})
+
+
+//auto upload new product
+exports.autoUploadNewProductData=functions.firestore.document('products/{productId}').onCreate((snap,context)=>{
+    const product=snap.data();
+    product.objectID=context.params.productId;
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(product);
+})
+//auto update existing data
+exports.autoUpdateProductData=functions.firestore.document('products/{productId}').onUpdate((change,context)=>{
+    const product=change.after.data();
+    product.objectID=context.params.productId;
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.saveObject(product);
+})
+//auto delete product
+exports.autoDeleteProductData=functions.firestore.document('products/{productId}').onDelete((snap,context)=>{
+    const productId=context.params.productId;
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+    return index.deleteObject(productId);
+})
+
+//auto upload new seller
+exports.autoUploadNewSellerData=functions.firestore.document('seller/{sellerId}').onCreate((snap,context)=>{
+    const seller=snap.data();
+    seller.objectID=context.params.sellerId;
+    const index = client.initIndex(ALGOLIA_INDEX_SELLER);
+    return index.saveObject(seller);
+})
+//auto update existing seller
+exports.autoUpdateSellerData=functions.firestore.document('seller/{sellerId}').onUpdate((change,context)=>{
+    const seller=change.after.data();
+    seller.objectID=context.params.sellerId;
+    const index = client.initIndex(ALGOLIA_INDEX_SELLER);
+    return index.saveObject(seller);
+})
+//auto delete SELLER
+exports.autoDeleteSellerData=functions.firestore.document('seller/{sellerId}').onDelete((snap,context)=>{
+    const sellerId=context.params.sellerId;
+    const index = client.initIndex(ALGOLIA_INDEX_SELLER);
+    return index.deleteObject(sellerId);
 })
